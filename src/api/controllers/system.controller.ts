8557f847fa4a5f1835e66acab418e78ba3ce1cec
F7022108 Matheus Bernardi da Silva <mbernardi.s@bb.com.br>
2025-01-25 00:17:19 -0300

    update route

--- src/api/controllers/system.controller.ts
+++ src/api/controllers/system.controller.ts
@@ -2,8 +2,14 @@ import * as pjson from "../../../package.json";
 import { Request, Response } from "express";
 
 export async function welcome(_: Request, res: Response): Promise<void> {
+  const { name, version, author, license, description, keywords } = pjson;
+
   res.send({
-    app: pjson.name,
-    version: pjson.version,
+    app: name,
+    version,
+    author,
+    license,
+    description,
+    keywords 
   });
 }
