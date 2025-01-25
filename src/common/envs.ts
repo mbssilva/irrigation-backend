8fbc8c315cc31cdc4daba64961cd0bfd97d8eeb0
F7022108 Matheus Bernardi da Silva <mbernardi.s@bb.com.br>
2025-01-24 21:38:39 -0300

    Corrige env. Prepara deploy

--- src/common/envs.ts
+++ src/common/envs.ts
@@ -1,5 +1,5 @@
 export const envs = {
-    appPort: process.env.APP_PORT || 3333,
+    appPort: process.env.APP_PORT ? Number(process.env.APP_PORT) : 3333,
     mongoURI: process.env.MONGODB_URI || '',
     securityToken: process.env.SECURITY_TOKEN || '',
 }
