46e9aac6e1569cd63d26080091b853df88464603
F7022108 Matheus Bernardi da Silva <mbernardi.s@bb.com.br>
2025-01-26 01:08:28 -0300

    Adjust regression

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 export function moistureLevelRegression(moistureLevel: number) {
-  return 1.0966 * Math.exp((-10.02 * moistureLevel) / 4095);
+  return 1.0966 * Math.exp(-10.02 * moistureLevel);
 }
